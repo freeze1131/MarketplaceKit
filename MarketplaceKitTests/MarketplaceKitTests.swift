741890c85d2d287b1532bb5661d038bc5b116b94
Aco <ozenahmetcemil@gmail.com>
2026-01-06 07:03:26 +0300

    Initial Commit

---
+++ MarketplaceKitTests/MarketplaceKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarketplaceKitTests.swift
+//  MarketplaceKitTests
+//
+//  Created by Ahmet Ozen on 6.01.2026.
+//
+
+import Testing
+@testable import MarketplaceKit
+
+struct MarketplaceKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
