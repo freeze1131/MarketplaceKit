b7624471bab2e31a9971c88970018f4938f1b533
Aco <ozenahmetcemil@gmail.com>
2026-01-06 07:20:40 +0300

    Initial commit

---
+++ MarketplaceKit/Views/ProductListView.swift
@@ -0,0 +1,19 @@
+//
+//  ProductListView.swift
+//  MarketplaceKit
+//
+//  Created by Ahmet Ozen on 6.01.2026.
+//
+
+import SwiftUI
+
+struct ProductListView: View {
+    var body: some View {
+        Text("Product List")
+            .font(.title)
+    }
+}
+
+#Preview {
+    ProductListView()
+}
